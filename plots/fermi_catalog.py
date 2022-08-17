f822ae7ab94bca0251d50a8052b24eb8adeab5ed
Anno Knierim <anno.knierim@tu-dortmund.de>
2022-08-17 14:02:17 +0200

    2022-08-17: Worked on ch.1

--- plots/fermi_catalog.py
+++ plots/fermi_catalog.py
@@ -1,14 +1,10 @@
-from enum import unique
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 
-from astropy.table import unique, Table
+from astropy.table import Table
 from astropy.coordinates import SkyCoord
-from astropy.io import fits
 
-import itertools
-markers = itertools.cycle(('o', 'v', '^', '<', '>', '8', 's', 'p', '*', 'h', 'H', 'D', 'd', 'P', 'X', ))
-
+# not used, but nice to have for lookups etc
 source_classes = {
     "agn": "AGN",
     "bcu": "AGN of uncertain type",
@@ -37,61 +33,50 @@ source_classes = {
 
 
 fits_image_filename = './plots/data/fermi_4fgl_dr3.fit'
-
 table = Table.read(fits_image_filename, hdu=1)
-
-
-# col = "CLASS1"
-# grouped = table.group_by(col)
-
-# fig, ax = plt.subplots(1, 1, figsize=(8,4.5), subplot_kw=dict(projection='mollweide'), constrained_layout=True)
-
-# for idx, key in enumerate(grouped.groups.keys[1:18]):
-#     # print(key, grouped.groups[idx])
-
-#     tab = grouped.groups[idx]
-#     tab.sort('Flux1000')
-
-#     coords = SkyCoord(tab["GLON"], tab["GLAT"], frame="galactic")
-
-#     x_coord = coords.galactic.l.wrap_at('180 deg').rad
-#     y_coord = coords.galactic.b.rad
-
-#     colors = grouped.groups[idx]
-
-#     ax.scatter(x_coord, y_coord, marker=next(markers), label=key, c=colors, cmap='inferno', norm=LogNorm(), alpha=0.9, s=12, edgecolors='none')
-
-# # cbar = fig.colorbar(sc, ax=ax, pad=0.1, fraction=0.025, label='Flux / \si{\TeV\per\square\centi\meter \per\second}')
-# ax.set_facecolor('black')
-# ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05),
-#           fancybox=True, shadow=True, ncol=5)
-
-# plt.savefig('./build/fermi_test.pdf', dpi=300, bbox_inches='tight')
-
-# grouped = grouped[grouped.groups == 0]
-# print(table.colnames)
-# unique = unique(table[["DataRelease", "Extended_Source_Name"]], "Extended_Source_Name")
-# print(unique)
-# print(table["Extended_Source_Name"][table["Extended_Source_Name"] == "Crab IC"])
-# crab = table[table["Extended_Source_Name"] == "gal"]
-
 
 table.sort('Flux1000')
 coords = SkyCoord(table['GLON'], table['GLAT'], frame='galactic')
 
-x_coord = coords.galactic.l.wrap_at('180 deg').rad
+# # just to get all unique extended source names
+# for idx, item in enumerate(table["Extended_Source_Name"]):
+#     if not item == 18*" ":
+#         print(f"{idx}: {item}")
+
+agn = []
+snr = []
+for idx, item in enumerate(table["CLASS2"]):
+    if "agn" in item:
+        agn.append(idx)
+    if "AGN" in item:
+        agn.append(idx)
+
+for idx, item in enumerate(table["CLASS1"]):
+    if "snr" in item:
+        snr.append(idx)
+    if "SNR" in item:
+        snr.append(idx)
+
+
+x_coord = -coords.galactic.l.wrap_at('180 deg').rad
 y_coord = coords.galactic.b.rad
 
 colors = table['Energy_Flux100'].to('TeV/(cm2 s)')
 
+
 fig, ax = plt.subplots(1, 1, figsize=(8,4.5), subplot_kw=dict(projection='mollweide'), constrained_layout=True)
+
 sc = ax.scatter(x_coord, y_coord, c=colors, cmap='inferno', norm=LogNorm(), alpha=0.9, s=8, edgecolors='none')
+ax.scatter(x_coord[6630], y_coord[6630], marker='o', s=20, facecolors='none', edgecolors='#fc0000', label='Crab IC')
+ax.scatter(x_coord[agn], y_coord[agn], marker='o', s=20, facecolors='none', edgecolors='#2ab5fa', label='AGN')
+ax.scatter(x_coord[snr], y_coord[snr], marker='o', s=20, facecolors='none', edgecolors='#33ff3a', label='SNR')
 
 ax.set_facecolor('black')
 
 ax.set_xticklabels([])
 ax.set_yticklabels([])
 ax.grid(False)
+ax.legend()
 
 ax.set_title('LAT 12-year Source Catalog (4FGL-DR3)')
 
